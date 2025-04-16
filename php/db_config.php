7be3a30ad9c3514d55a0c10cfae3d7df63316937
rodri1gg <rodrigomachado532@gmail.com>
2025-04-15 21:54:25 -0300

    config bd

---
+++ php/db_config.php
@@ -0,0 +1,13 @@
+<?php
+$servername = getenv('DB_HOST');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASS');
+$dbname = getenv('DB_NAME');
+$port = getenv('DB_PORT');
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+?>
